5495ff5b0028fe35f88caf824a0959eb51a3f9dc
Johan Muñoz <jmunoz41@uan.edu.co>
2021-01-16 18:04:26 -0500

    se adiere pantalla registro con iconos colores y logo, solo falta hacer scroll para poder subir y najar dentro de la pantalla

--- app/src/main/java/com/Alikapp/alikappconductor/CostumerRegistroActivity.java
+++ app/src/main/java/com/Alikapp/alikappconductor/CostumerRegistroActivity.java
@@ -2,19 +2,24 @@ package com.Alikapp.alikappconductor;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static com.paypal.android.sdk.ey.v;
 
 public class CostumerRegistroActivity  extends AppCompatActivity {
 
     private EditText mEmail, mPassword;
-    private Button  mRegistration;
+    private Button  mRegistro;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
-
-
 
 
 }
