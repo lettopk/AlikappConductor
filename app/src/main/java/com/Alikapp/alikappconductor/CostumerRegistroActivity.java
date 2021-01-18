c4a6ca1ed3effee24469af807bcce16f04b607ef
Johan Muñoz <jmunoz41@uan.edu.co>
2021-01-18 11:37:43 -0500

    se añade la api key registrada a la rama login

--- app/src/main/java/com/Alikapp/alikappconductor/CostumerRegistroActivity.java
+++ app/src/main/java/com/Alikapp/alikappconductor/CostumerRegistroActivity.java
@@ -1,5 +1,6 @@
 package com.Alikapp.alikappconductor;
 
+import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,9 +19,30 @@ import static com.paypal.android.sdk.ey.v;
 public class CostumerRegistroActivity  extends AppCompatActivity {
 
     private EditText mEmail, mPassword;
-    private Button  mRegistro;
+    private Button mRegistration;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    mAuth = FirebaseAuth.getInstance();
+
+    firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@androidx.annotation.NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if(user!=null){
+                Intent intent = new Intent(CustomerLoginActivity.this, CustomerMapActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        }
+    };
+
+    mEmail = (EditText) findViewById(R.id.email);
+    mPassword = (EditText) findViewById(R.id.password);
+
+
+    mRegistration = (Button) findViewById(R.id.registration);
 
 
 }
