04bdd4f4dc1e09ad78dd2b9c2f0d0dade57854c6
Johan munoz <jmunoz41@uan.edu.co>
2021-04-12 15:17:27 -0500

    actualizacion

--- app/src/main/java/com/Alikapp/alikappconductor/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/Alikapp/alikappconductor/ui/slideshow/SlideshowFragment.java
@@ -4,12 +4,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import com.Alikapp.alikappconductor.CashActivity;
-import com.Alikapp.alikappconductor.HistoryActivity;
-import com.Alikapp.alikappconductor.LegalActivity;
-import com.google.firebase.auth.FirebaseAuth;
-import com.Alikapp.alikappconductor.CustomerLoginActivity;
-import com.Alikapp.alikappconductor.CustomerMapActivity;
+import com.Alikapp.alikappconductor.ActivityBilletera;
 import com.Alikapp.alikappconductor.R;
 
 public class SlideshowFragment extends androidx.fragment.app.Fragment {
@@ -18,7 +13,7 @@ public class SlideshowFragment extends androidx.fragment.app.Fragment {
                                           ViewGroup container, android.os.Bundle savedInstanceState) {
         android.view.View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
 
-        Intent intent = new Intent(getContext(), CashActivity.class);
+        Intent intent = new Intent(getContext(), ActivityBilletera.class);
         startActivity(intent);
 
         return root;
