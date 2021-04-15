2d4f2a0a2496568ba343a6e4cea6b84348a047eb
Johan munoz <jmunoz41@uan.edu.co>
2021-04-15 18:41:24 -0500

    Actualizacion metodo de pago

--- app/src/main/java/com/Alikapp/alikappconductor/popupRecarga.java
+++ app/src/main/java/com/Alikapp/alikappconductor/popupRecarga.java
@@ -1,17 +1,31 @@
 package com.Alikapp.alikappconductor;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class popupRecarga extends AppCompatActivity {
 
+    private Button mMetRecarga;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_popup_recarga);
+
+        mMetRecarga = findViewById(R.id.metPagoBancolo);
+        mMetRecarga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent( popupRecarga.this, Submit_card_view.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
