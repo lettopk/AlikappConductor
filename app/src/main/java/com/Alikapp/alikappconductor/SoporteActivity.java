0ff6c553084dbb208dc35b084e87e96a304ce2ca
GaleanoJorge <juandavidrianoperez@gmail.com>
2021-06-28 19:08:48 -0500

    actualización

--- app/src/main/java/com/Alikapp/alikappconductor/SoporteActivity.java
+++ app/src/main/java/com/Alikapp/alikappconductor/SoporteActivity.java
@@ -3,6 +3,8 @@ package com.Alikapp.alikappconductor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SoporteActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SoporteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soporte);
+
+        Button acepto = findViewById(R.id.acepto);
+        acepto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
