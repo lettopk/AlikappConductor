e3abbfc09f54edda28a4f37f52287375a83069d3
GaleanoJorge <juandavidrianoperez@gmail.com>
2021-06-28 22:01:19 -0500

    actualizaciones

--- app/src/main/java/com/Alikapp/alikappconductor/SoporteActivity.java
+++ app/src/main/java/com/Alikapp/alikappconductor/SoporteActivity.java
@@ -2,9 +2,12 @@ package com.Alikapp.alikappconductor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class SoporteActivity extends AppCompatActivity {
 
@@ -18,6 +21,24 @@ public class SoporteActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        LinearLayout enlaceFacebook = findViewById(R.id.enlaceFacebook);
+        enlaceFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://m.facebook.com/AlikappUM/"));
+                startActivity(intent);
+            }
+        });
+
+        LinearLayout enlaceInstagram = findViewById(R.id.enlaceInstagram);
+        enlaceInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/alikapp_um/"));
+                startActivity(intent);
             }
         });
     }
